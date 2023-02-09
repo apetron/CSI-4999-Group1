838fb22cb0d6bb3b21e6b456eb002415efbcb025
apetron <apetron@oakland.edu>
2023-02-09 11:45:35 -0500

    Checks user on message

--- Bot.py
+++ Bot.py
@@ -21,6 +21,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
     UrlDetection.checkUrls(message) #check the type of the message
 
 
