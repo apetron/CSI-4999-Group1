4236efb1726af173d7b80e8e4a0821353c795b64
apetron <apetron@oakland.edu>
2023-02-09 11:36:25 -0500

    Started working in python for the bot

---
+++ Bot.py
@@ -0,0 +1,27 @@
+import os
+import UrlDetection
+import discord
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    UrlDetection.checkUrls(message) #check the type of the message
+
+
+client.run(TOKEN)
--- UrlDetection/UrlDetection.py
+++ UrlDetection/UrlDetection.py
@@ -47,7 +47,7 @@ def getUrlDomain(url, type):
     print("The domain is: " + finalDomain)
     return finalDomain 
     
-def validateUrl(type, url):
+def validateUrl(type, url, mode, domains):
     domain = getUrlDomain(url, type)
     print(domain)
 
@@ -55,34 +55,37 @@ def validateUrl(type, url):
         #print("url" if domain not in domains else "not")
         if domain not in domains:
             print("Url is not allowed")
-            #Delete message
+            return False
         else:
             print("Url is allowed")
-            #Possibly remove else block
+            return True
     elif mode == "blacklist":
         if domain in domains:
             print("Url is not allowed")
-            #Delete message
+            return False
         else:
             print("Url is allowed")
-            #Possibly remove else block
+            return True
     else:
         print("Config is invalid, must be either set to 'whitelist' or 'blacklist'")
 
     
 
 def checkUrls(text):
-    nlp = spacy.load("en_core_web_sm")
-    msg = nlp(text)
-    for token in msg:
-        type = urlType(token.text)
-        if not type == "n":
-            validateUrl(type, token.text)
+    #This may not be optimal, it might be better to load the config settings somewhere within the bot's code
+    #Then just pass them into this function. Might not want to load from a file everytime a message is sent in discord
+    mode, domains = loadConfigSettings("UrlDetection/domainConfig.json")
+    if mode == "whitelist" or mode == "blacklist":
+        nlp = spacy.load("en_core_web_sm")
+        msg = nlp(text)
+        for token in msg:
+            type = urlType(token.text)
+            if not type == "n":
+                validateUrl(type, token.text, mode, domains)
+    else:
+        raise ValueError("Error: the url detection mode is not in a valid configuraion. It must be either whitelist or blacklist")
     
 
-mode, domains = loadConfigSettings("UrlDetection/domainConfig.json")
-print(mode)
-print(domains)
 url = "https://mail.google.com" #example, test with a bunch
 url1 = "http://apps.js.medium.co.uk:80/artificialis/phishing-url_detection_with-python-a2c3dd3a87e8?test=7"
 url2 = "https://drive.google.com/drive/u/0/folders/1XWUjjkndB6pD7Bt79pJKM3cVyNgX0JMf"
